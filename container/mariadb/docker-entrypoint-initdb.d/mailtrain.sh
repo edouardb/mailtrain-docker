999330630476ef58af9f53b3439ec5b17c1ad248
Edouard BONLIEU <ebonlieu@ocs.online.net>
2016-12-16 13:38:48 +0100

    Rename vars

--- container/mariadb/docker-entrypoint-initdb.d/mailtrain.sh
+++ container/mariadb/docker-entrypoint-initdb.d/mailtrain.sh
@@ -6,7 +6,7 @@ INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('admin_email','$MAILTRAIN_
 INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('default_address','$MAILTRAIN_DEFAULT_EMAIL') ON DUPLICATE KEY UPDATE \`value\`='$MAILTRAIN_DEFAULT_EMAIL';
 INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_hostname','localhost') ON DUPLICATE KEY UPDATE \`value\`='localhost';
 INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_disable_auth','') ON DUPLICATE KEY UPDATE \`value\`='';
-INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_user','$MAILTRAIN_USER') ON DUPLICATE KEY UPDATE \`value\`='$MAILTRAIN_USER';
+INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_user','$SMTP_USER') ON DUPLICATE KEY UPDATE \`value\`='$SMTP_USER';
 INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_pass','$SMTP_PASS') ON DUPLICATE KEY UPDATE \`value\`='$SMTP_PASS';
 INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_encryption','$SMTP_ENCRYPTION') ON DUPLICATE KEY UPDATE \`value\`='$SMTP_ENCRYPTION';
 INSERT INTO \`settings\` (\`key\`, \`value\`) VALUES ('smtp_port','$SMTP_PORT') ON DUPLICATE KEY UPDATE \`value\`='$SMTP_PORT';
